40839bbff8736c918af17d0cf977070a1a3771d3
Bogdan Danciu <danciu_bogdan94@yahoo.com>
2023-04-18 17:18:29 +0200

    Update README and data generation files

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # flow_reconstruction
+
+## Project branch structure considerations
+Branches:
+- master branch: any code in the master branch should be working
+- feature branch: create new descriptively-named branches off the main branch for new work, such as feature/add_variable_percentage
+Good practice (not mandatory):
+- Commit new work to your local branches and regularly push work to the remote
+- To request feedback or help, or when you think your work is ready to merge into the master branch, open a pull request.
+- After your work or feature has been reviewed and approved, it can be merged into the master branch.
+
---
+++ generate_data/read_vtp_slice.py
@@ -0,0 +1,33 @@
+import sys
+import time
+import numpy as np
+import vtk as vtk
+sys.path.append('../src/')
+
+t00 = time.time()
+
+# Specify source file:
+fnamevtk = 'slice01.vtp'
+
+# Reference values for conversion:
+xref = 0.086          # [m]
+uref = 11.72571875    # [m/s] max piston speed, 2500 rpm
+tref = 333.15         # [K]
+
+
+#----------------- VTK file ---------------------
+print(f'\n>>>> Reading VTK file {fnamevtk}', flush=True)
+t0 = time.time()
+reader = vtk.vtkXMLPolyDataReader()
+reader.SetFileName(fnamevtk)
+reader.Update()
+data_in = reader.GetOutput()
+t1 = time.time()
+print(f'Time elapsed [sec]: {t1-t0:.1f}', flush=True)
+
+points = np.array(data_in.GetPoints().GetData())
+x_vel = np.array(data_in.GetPointData().GetArray("x_velocity"))
+y_vel = np.array(data_in.GetPointData().GetArray("y_velocity"))
+z_vel = np.array(data_in.GetPointData().GetArray("z_velocity"))
+temp = np.array(data_in.GetPointData().GetArray("temperature"))
+

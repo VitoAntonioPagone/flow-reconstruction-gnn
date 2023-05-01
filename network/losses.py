14500972829ce988f0f5572577aeb7bedbafdb6b
VitoPagone <cristianpagone1@gmail.com>
2023-05-02 00:42:12 +0200

    Modified NS loss and removed CombinedLoss which has still to be tested as class

--- network/losses.py
+++ network/losses.py
@@ -14,15 +14,16 @@ class MaskedMSELoss(Module):
         loss_value = torch.mean(masked_diff ** 2)
         return loss_value
 
-class NavierStokesLoss:
+class NavierStokesLoss(Module):
     def __init__(self, model, rho=1.0, mu=1.0, delta=1e-3):
+        super().__init__()
         self.model = model
         self.rho = rho
         self.mu = mu
         self.delta = delta
 
     def forward(self, pred, x, y, temp):
-        u, v, w, _ = torch.split(pred, 1, dim=1)
+        u, v, w = torch.split(pred, 1, dim=1)
 
         du_dx, du_dy = self._get_derivatives(u, x, y, temp)
         dv_dx, dv_dy = self._get_derivatives(v, x, y, temp)
@@ -53,24 +54,14 @@ class NavierStokesLoss:
         return NS_loss + div_loss
 
     def _get_derivatives(self, f, x, y, temp):
-        df_dx = (self.model(torch.cat((x + self.delta, y, temp), dim=1))[:, 0] - f) / self.delta
-        df_dy = (self.model(torch.cat((x, y + self.delta, temp), dim=1))[:, 0] - f) / self.delta
+        df_dx = (self.model(torch.cat((x + self.delta, y, temp), dim=-1))[:, 0] - f) / self.delta
+        df_dy = (self.model(torch.cat((x, y + self.delta, temp), dim=-1))[:, 0] - f) / self.delta
         return df_dx, df_dy
 
     def _get_second_derivatives(self, f, x, y, temp):
-        d2f_dx2 = (self.model(torch.cat((x + 2 * self.delta, y, temp), dim=1))[:, 0] - 2 * self.model(torch.cat((x + self.delta, y, temp), dim=1))[:, 0] + f) / (self.delta ** 2)
-        d2f_dy2 = (self.model(torch.cat((x, y + 2 * self.delta, temp), dim=1))[:, 0] - 2 * self.model(torch.cat((x, y + self.delta, temp), dim=1))[:, 0] + f) / (self.delta ** 2)
+        d2f_dx2 = (self.model(torch.cat((x + 2 * self.delta, y, temp), dim=-1))[:, 0] - 2 * self.model(torch.cat((x + self.delta, y, temp), dim=-1))[:, 0] + f) / (self.delta ** 2)
+        d2f_dy2 = (self.model(torch.cat((x, y + 2 * self.delta, temp), dim=-1))[:, 0] - 2 * self.model(torch.cat((x, y + self.delta, temp), dim=-1))[:, 0] + f) / (self.delta ** 2)
         return d2f_dx2, d2f_dy2
-    
-class CombinedLoss:
-    def __init__(self, alpha, masked_mse_loss, navier_stokes_loss):
-        self.alpha = alpha
-        self.masked_mse_loss = masked_mse_loss
-        self.navier_stokes_loss = navier_stokes_loss
 
-    def forward(self, pred, target, mask, x, y, temp):
-        mse_loss = self.masked_mse_loss(pred, target, mask)
-        ns_loss = self.navier_stokes_loss.forward(pred, x, y, temp)
-        combined_loss = self.alpha * mse_loss + (self.alpha) * ns_loss
-        return combined_loss
+    
 

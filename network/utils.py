4a956a8b0881428b0d9efd6fc097fb0e2495e23f
VitoPagone <cristianpagone1@gmail.com>
2023-05-02 00:45:51 +0200

    Added get_loaders_mlp

--- network/utils.py
+++ network/utils.py
@@ -1,5 +1,5 @@
 import torch
-from datasets import FlowDataset
+from datasets import FlowDataset, MLPDataset
 from torch.utils.data import DataLoader
 from losses import MaskedMSELoss
 import matplotlib.pyplot as plt
@@ -52,6 +52,34 @@ def get_loaders(
 
     return train_loader, val_loader
 
+def get_loaders_mlp(
+    train_data_file,
+    val_data_file,
+    batch_size,
+    num_workers=8,
+    pin_memory=True,
+):
+    train_ds = MLPDataset(train_data_file)
+
+    train_loader = DataLoader(
+        train_ds,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        shuffle=True,
+    )
+
+    val_ds = MLPDataset(val_data_file)
+
+    val_loader = DataLoader(
+        val_ds,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_memory,
+        shuffle=False,
+    )
+
+    return train_loader, val_loader
 
 def check_accuracy(loader, model, device="cuda"):
     model.eval()
@@ -69,6 +97,27 @@ def check_accuracy(loader, model, device="cuda"):
 
     model.train()
 
+def check_accuracy_mlp(loader, model, mse_loss, navier_stokes_loss, alpha, device="cuda"):
+    model.eval()
+    total_loss = 0
+    count = 0
+
+    with torch.no_grad():
+        for inputs, labels in loader:
+            inputs, labels = inputs.to(device), labels.to(device)
+            preds = model(inputs)
+            loss_mse = mse_loss(preds, labels)
+            loss_ns = navier_stokes_loss.forward(preds, *torch.split(inputs, 1, dim=1))
+            loss = loss_mse + alpha * loss_ns
+            total_loss += loss.item()
+            count += 1
+
+    avg_loss = total_loss / count
+    print(f"Validation Loss: {avg_loss:.4f}")
+
+    model.train()
+
+
 def plot_losses(train_losses, val_losses):
     plt.figure()
     plt.plot(train_losses, label="Training Loss")
@@ -78,3 +127,12 @@ def plot_losses(train_losses, val_losses):
     plt.legend()
     plt.show()
 
+import torch
+
+def initialize_weights(model):
+    for module in model.modules():
+        if isinstance(module, (torch.nn.Conv2d, torch.nn.Linear)):
+            torch.nn.init.xavier_uniform_(module.weight)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+    return print('Weights initialized with Glorot (Xavier) initializer, bias initialized to zero')

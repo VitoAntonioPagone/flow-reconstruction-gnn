30a98915b772397a26718d4283d2e40718c6bf74
VitoPagone <cristianpagone1@gmail.com>
2023-05-04 16:38:52 +0200

    MLP Combined datasets creation

--- utils/mlp_combined_datasets.py
+++ utils/mlp_combined_datasets.py
@@ -2,14 +2,14 @@ import os
 import numpy as np
 
 # Set the paths to original train, test, and validation folders
-train_input_dir = "flow_reconstruction/dataset_mlp/npz_data/train"
-test_input_dir = "flow_reconstruction/dataset_mlp/npz_data/test"
-val_input_dir = "flow_reconstruction/dataset_mlp/npz_data/validation"
+train_input_dir = "flow_reconstruction/dataset_mlp/original_data/npz_data/train/"
+test_input_dir = "flow_reconstruction/dataset_mlp/original_data/npz_data/test/"
+val_input_dir = "flow_reconstruction/dataset_mlp/original_data/npz_data/validation/"
 
 # Set the paths to the output folders
-train_output_dir = "flow_reconstruction/dataset_mlp/train_data/train_combined"
-test_output_dir = "flow_reconstruction/dataset_mlp/train_data/test_combined"
-val_output_dir = "flow_reconstruction/dataset_mlp/train_data/validation_combined"
+train_output_dir = "flow_reconstruction/dataset_mlp/train_data/train_combined/"
+test_output_dir = "flow_reconstruction/dataset_mlp/train_data/test_combined/"
+val_output_dir = "flow_reconstruction/dataset_mlp/train_data/validation_combined/"
 
 # Create output directories if they do not exist
 os.makedirs(train_output_dir, exist_ok=True)
@@ -20,11 +20,14 @@ os.makedirs(val_output_dir, exist_ok=True)
 def concatenate_npz_files(npz_files, input_dir):
     combined_data = []
     for npz_file in npz_files:
+        print(f"Analyzing file: {npz_file}")  # Print the file being analyzed
         file_path = os.path.join(input_dir, npz_file)
-        data = np.load(file_path)["data"]
-        combined_data.append(data)
+        data = np.load(file_path)
+        combined_array = np.column_stack([data[key] for key in data.keys()])
+        combined_data.append(combined_array)
 
     return np.vstack(combined_data)
+
 
 # Define a function to process data for a given input directory and output directory
 def process_data(input_dir, output_dir):

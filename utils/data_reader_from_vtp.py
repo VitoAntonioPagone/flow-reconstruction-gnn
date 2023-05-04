22af893018feff0e6126f41c060efdde1d182cb2
VitoPagone <cristianpagone1@gmail.com>
2023-05-04 16:12:58 +0200

    MLP data reader from vtp

--- utils/data_reader_from_vtp.py
+++ utils/data_reader_from_vtp.py
@@ -1,70 +1,34 @@
 import os
 import glob
-import time
 import numpy as np
 import vtk
-from vtk.util import numpy_support
+
+def read_vtp_slice(file_name):
+    reader = vtk.vtkXMLPolyDataReader()
+    reader.SetFileName(file_name)
+    reader.Update()
+    data_in = reader.GetOutput()
+
+    data_out = {
+        'x': np.array(data_in.GetPoints().GetData())[:, 0],
+        'y': np.array(data_in.GetPoints().GetData())[:, 1],
+        'x_velocity': np.array(data_in.GetPointData().GetArray("x_velocity")),
+        'y_velocity': np.array(data_in.GetPointData().GetArray("y_velocity")),
+        'z_velocity': np.array(data_in.GetPointData().GetArray("z_velocity")),
+        'temperature': np.array(data_in.GetPointData().GetArray("temperature"))}
+
+    return data_out
+
+def vtp_to_npz(input_folder, output_folder):
+    os.makedirs(output_folder, exist_ok=True)
+
+    for vtp_file in glob.glob(os.path.join(input_folder, "*.vtp")):
+        print(f"Processing file: {vtp_file}")
+        data = read_vtp_slice(vtp_file)
+        output_file = os.path.join(output_folder, os.path.splitext(os.path.basename(vtp_file))[0] + ".npz")
+        np.savez(output_file, **data)
 
 
-
-INPUT_TRAIN_FODLER = 'flow_reconstruction/dataset/train/vtp_files'
-OUTPUT_TRAIN_FOLDER = 'flow_reconstruction/dataset/train/csvs'
-INPUT_TEST_FOLDER = 'flow_reconstruction/dataset/test/vtp_files'
-OUTPUT_TEST_FOLDER = 'flow_reconstruction/dataset/test/csvs'
-
-
-def vtk_to_csv(input_folder, output_folder):
-    if not os.path.exists(output_folder):
-        os.makedirs(output_folder)
-
-    vtp_files = glob.glob(os.path.join(input_folder, '*.vtp'))
-
-    for fnamevtk in vtp_files:
-        t00 = time.time()
-
-        print(f'\n>>>> Reading VTK file {fnamevtk}', flush=True)
-        t0 = time.time()
-        reader = vtk.vtkXMLPolyDataReader()
-        reader.SetFileName(fnamevtk)
-        reader.Update()
-        data_in = reader.GetOutput()
-        t1 = time.time()
-        print(f'Time elapsed [sec]: {t1-t0:.1f}', flush=True)
-
-        points = numpy_support.vtk_to_numpy(data_in.GetPoints().GetData())
-
-        point_data_arrays = [
-            data_in.GetPointData().GetArray(i)
-            for i in range(data_in.GetPointData().GetNumberOfArrays())
-            if data_in.GetPointData().GetArray(i).GetName() != 'avtOriginalNodeNumbers'
-        ]
-
-        np_arrays = [
-            numpy_support.vtk_to_numpy(vtk_array)
-            for vtk_array in point_data_arrays
-        ]
-
-        feature_names = ['x', 'y'] + [
-            arr.GetName() for arr in point_data_arrays
-        ]
-
-        output_file = os.path.join(
-            output_folder, os.path.splitext(os.path.basename(fnamevtk))[0] + '.csv'
-        )
-
-        with open(output_file, 'w') as f:
-            f.write(','.join(feature_names) + '\n')
-
-            for i in range(points.shape[0]):
-                values = (
-                    [str(points[i, 0]), str(points[i, 1])]
-                    + [str(np_array[i]) for np_array in np_arrays]
-                )
-                f.write(','.join(values) + '\n')
-
-        print(f"Converted {fnamevtk} to {output_file}")
-
-
-if __name__ == "__main__":
-    vtk_to_csv(INPUT_TRAIN_FODLER, OUTPUT_TRAIN_FOLDER)
-    vtk_to_csv(INPUT_TEST_FOLDER, OUTPUT_TEST_FOLDER)
+# Convert train and test vtp files to npz files
+vtp_to_npz("flow_reconstruction/dataset_mlp/original_data/train", "flow_reconstruction/dataset_mlp/original_data/npz_data/train")
+vtp_to_npz("flow_reconstruction/dataset_mlp/original_data/test", "flow_reconstruction/dataset_mlp/original_data/npz_data/test")

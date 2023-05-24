c66c13a2fe4bf925d658d86dfc48ea8839afcefb
VitoPagone <cristianpagone1@gmail.com>
2023-05-24 12:27:26 +0200

    Added relative path, and removed MLP code

--- network/predict.py
+++ network/predict.py
@@ -12,10 +12,10 @@ from utils import load_checkpoint
 
 def run_autoencoder():
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-    CHECKPOINT_FILE = 'flow_reconstruction/network/trained_models/autoencoder_checkpoint.pth.tar'
-    TEST_INPUTS_DIR = 'flow_reconstruction/dataset/train_data/test_inputs_50/'
-    TEST_LABELS_DIR = 'flow_reconstruction/dataset/train_data/test_labels_50/'
-    TEST_FILE_NUM = 1
+    CHECKPOINT_FILE = '../trained_models/autoencoder_checkpoint_alpha_1_beta_10_lr_0.001_batch_128.pth.tar'
+    TEST_INPUTS_DIR = '../dataset/train_data/test_inputs_50/'
+    TEST_LABELS_DIR = '../dataset/train_data/test_labels_50/'
+    TEST_FILE_NUM = 8
 
 
     def reconstruct_flow(model, input_tensor, missing_mask_tensor):
@@ -41,14 +41,14 @@ def run_autoencoder():
         test_missing_mask_tensor = test_missing_mask_tensor.unsqueeze(0)
         # Load pre-trained model
         model = ConvAutoEncoder().to(DEVICE)
-        load_checkpoint(torch.load(CHECKPOINT_FILE), model)
+        load_checkpoint(torch.load(CHECKPOINT_FILE, map_location=torch.device(DEVICE)), model)
         reconstructed_flow_tensor = reconstruct_flow(model, test_input_tensor, test_missing_mask_tensor)
         reconstructed_flow_tensor = reconstructed_flow_tensor.cpu()  # Move the tensor back to CPU for visualization
 
-        fig, axes = plt.subplots(3, 4, figsize=(12, 8), dpi=100)  
+        fig, axes = plt.subplots(3, 3, figsize=(12, 8), dpi=120)  
         fig.subplots_adjust(hspace=0.5, wspace=0.5)  
 
-        for i in range(4):
+        for i in range(3):
             # Find min and max values of the ground truth tensor for the current channel
             vmin = test_label_tensor[0, i].min()
             vmax = test_label_tensor[0, i].max()
@@ -75,37 +75,6 @@ def run_autoencoder():
         plt.show()
     predict()
 
-
-def run_mlp():
-
-
-    INPUT = '/Users/vitoantonio/Desktop/feature_network_architectures/flow_reconstruction/dataset_mlp/interpolated/interpolated_test/interpolated_cyc10_CAD615_Y0_Z0_X0.npz'
-    LABEL = '/Users/vitoantonio/Desktop/feature_network_architectures/flow_reconstruction/dataset_mlp/interpolated/interpolated_reconstructed_mlp/interpolated_cyc10_CAD615_Y0_Z0_X0.npz'
-
-
-    def compare_channels(file1, file2):
-        data1 = np.load(file1)
-        data2 = np.load(file2)
-
-        common_keys = set(data1.files).intersection(data2.files)
-
-        for key in common_keys:
-            fig, axes = plt.subplots(1, 2, figsize=(15, 5))
-
-            # Plot data from file1
-            im1 = axes[0].imshow(data1[key], cmap='jet', aspect='auto')
-            axes[0].set_title(f'File1: {key}')
-            cbar1 = fig.colorbar(im1, ax=axes[0])
-            cbar1.set_label('Values')
-
-            # Plot data from file2
-            im2 = axes[1].imshow(data2[key], cmap='jet', aspect='auto')
-            axes[1].set_title(f'File2: {key}')
-            cbar2 = fig.colorbar(im2, ax=axes[1])
-            cbar2.set_label('Values')
-
-            plt.show()
-    compare_channels(INPUT, LABEL)
 
 if __name__ == "__main__":
     run_autoencoder()

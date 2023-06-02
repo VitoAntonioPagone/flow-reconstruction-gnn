aaf99de53e61ed389f023d0e57016d166a3e4051
VitoPagone <cristianpagone1@gmail.com>
2023-06-02 16:18:48 +0200

    Updates of attention module

--- network/attention.py
+++ network/attention.py
@@ -3,15 +3,18 @@ import torch
 import torch.nn.functional as F
 # Squeeze-and-Excitation Layer (SELayer): adaptively recalibrates channel-wise feature responses by explicitly modeling interdependencies between channels
 
-class SELayer(nn.Module): # working ok
-    def __init__(self, channel, reduction=16):
-        super(SELayer, self).__init__()
+
+# Spatial Attention (SA): focuses on learning spatial dependencies and computes a spatial attention map by pooling channel information
+
+class ChannelAttention(nn.Module):
+    def __init__(self, num_channels, reduction_ratio=16):
+        super(ChannelAttention, self).__init__()
+
         self.avg_pool = nn.AdaptiveAvgPool2d(1)
         self.fc = nn.Sequential(
-            nn.Linear(channel, channel // reduction, bias=False),
+            nn.Linear(num_channels, num_channels // reduction_ratio, bias=False),
             nn.ReLU(inplace=True),
-            nn.Linear(channel // reduction, channel, bias=False),
-            nn.Sigmoid()
+            nn.Linear(num_channels // reduction_ratio, num_channels, bias=False),
         )
 
     def forward(self, x):
@@ -20,24 +23,6 @@ class SELayer(nn.Module): # working ok
         y = self.fc(y).view(b, c, 1, 1)
         return x * y.expand_as(x)
 
-# Spatial Attention (SA): focuses on learning spatial dependencies and computes a spatial attention map by pooling channel information
-
-class SpatialAttention(nn.Module): # working ok
-    def __init__(self, kernel_size=7):
-        super(SpatialAttention, self).__init__()
-
-        assert kernel_size in (3, 7), 'kernel size must be 3 or 7'
-        padding = 3 if kernel_size == 7 else 1
-
-        self.conv1 = nn.Conv2d(2, 1, kernel_size, padding=padding, bias=False)
-        self.sigmoid = nn.Sigmoid()
-
-    def forward(self, x):
-        avg_out = torch.mean(x, dim=1, keepdim=True)
-        max_out, _ = torch.max(x, dim=1, keepdim=True)
-        x = torch.cat([avg_out, max_out], dim=1)
-        x = self.conv1(x)
-        return self.sigmoid(x)
 
 
 

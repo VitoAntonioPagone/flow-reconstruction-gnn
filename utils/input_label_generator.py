b5460f11096325512a22668a6c3ef28d1136441a
VitoPagone <cristianpagone1@gmail.com>
2023-06-03 15:41:10 +0200

    Pushing everything in order to work in parallel on home PC also

--- utils/input_label_generator.py
+++ utils/input_label_generator.py
@@ -4,13 +4,13 @@ import os
 from pathlib import Path
 
 
-PERCENT_TO_REMOVE = 50
-INPUT_TRAIN_DIR = 'flow_reconstruction/dataset/npz_data/train/'
-INPUT_TEST_DIR = 'flow_reconstruction/dataset/npz_data/test/'
-OUTPUT_TRAIN_INPUTS_DIR = f'flow_reconstruction/dataset/train_data/train_inputs_{PERCENT_TO_REMOVE}/'
-OUTPUT_TEST_INPUTS_DIR = f'flow_reconstruction/dataset/train_data/test_inputs_{PERCENT_TO_REMOVE}/'
-OUTPUT_TRAIN_LABELS_DIR = f'flow_reconstruction/dataset/train_data/train_labels_{PERCENT_TO_REMOVE}/'
-OUTPUT_TEST_LABELS_DIR = f'flow_reconstruction/dataset/train_data/test_labels_{PERCENT_TO_REMOVE}/'
+PERCENT_TO_REMOVE = 90
+INPUT_TRAIN_DIR = '/Users/vitoantonio/Desktop/feature_network_architectures/flow_reconstruction/dataset/npz_data/train/'
+INPUT_TEST_DIR = '/Users/vitoantonio/Desktop/feature_network_architectures/flow_reconstruction/dataset/npz_data/test/'
+OUTPUT_TRAIN_INPUTS_DIR = f'/Users/vitoantonio/Desktop/feature_network_architectures/flow_reconstruction/dataset/train_data_{PERCENT_TO_REMOVE}/train_inputs_{PERCENT_TO_REMOVE}/'
+OUTPUT_TEST_INPUTS_DIR = f'/Users/vitoantonio/Desktop/feature_network_architectures/flow_reconstruction/dataset/train_data_{PERCENT_TO_REMOVE}/test_inputs_{PERCENT_TO_REMOVE}/'
+OUTPUT_TRAIN_LABELS_DIR = f'/Users/vitoantonio/Desktop/feature_network_architectures/flow_reconstruction/dataset/train_data_{PERCENT_TO_REMOVE}/train_labels_{PERCENT_TO_REMOVE}/'
+OUTPUT_TEST_LABELS_DIR = f'/Users/vitoantonio/Desktop/feature_network_architectures/flow_reconstruction/dataset/train_data_{PERCENT_TO_REMOVE}</test_labels_{PERCENT_TO_REMOVE}/'
 
 
 def create_inputs_labels(input_dir, output_dir_input, output_dir_labels, percent_to_remove):
@@ -49,4 +49,4 @@ def create_inputs_labels(input_dir, output_dir_input, output_dir_labels, percent
 
 if __name__ == "__main__":
     create_inputs_labels(INPUT_TRAIN_DIR, OUTPUT_TRAIN_INPUTS_DIR, OUTPUT_TRAIN_LABELS_DIR, PERCENT_TO_REMOVE)
-    create_inputs_labels(INPUT_TEST_DIR, OUTPUT_TEST_INPUTS_DIR, OUTPUT_TEST_LABELS_DIR, PERCENT_TO_REMOVE)
+    create_inputs_labels(INPUT_TEST_DIR , OUTPUT_TEST_INPUTS_DIR , OUTPUT_TEST_LABELS_DIR , PERCENT_TO_REMOVE)

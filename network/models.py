ee72727f66708f54a2b399f34ee89ab543cddb5c
VitoPagone <cristianpagone1@gmail.com>
2023-06-05 17:51:50 +0200

    Added working dilated convolution autoencoder, Autoencoder 50% working good, new architecture in test for 90%

--- network/models.py
+++ network/models.py
@@ -11,9 +11,114 @@ from torch_geometric.nn import GCNConv
 import torch
 from torch import nn
 
-class ConvAutoEncoder_simplified(nn.Module):
+class DilatedConvAutoEncoder(nn.Module):
     def __init__(self):
-        super(ConvAutoEncoder_simplified, self).__init__()
+        super(DilatedConvAutoEncoder, self).__init__()
+
+        # Encoder
+        self.encoder1 = nn.Sequential(
+            nn.Conv2d(4, 32*2, kernel_size=7, stride=1, padding=6, dilation=2),
+            nn.ReLU()
+        )
+        self.encoder2 = nn.Sequential(
+            nn.Conv2d(32*2, 64*2, kernel_size=7, stride=1, padding=6, dilation=2),
+            nn.ReLU()
+        )
+        self.encoder3 = nn.Sequential(
+            nn.Conv2d(64*2, 128*2, kernel_size=7, stride=1, padding=6, dilation=2),
+            nn.ReLU()
+        )
+        self.encoder4 = nn.Sequential(
+            nn.Conv2d(128*2, 256*2, kernel_size=7, stride=1, padding=6, dilation=2),
+            nn.ReLU()
+        )
+
+        # Decoder
+        self.decoder4 = nn.Sequential(
+            nn.ConvTranspose2d(384*2, 128*2, kernel_size=7, stride=1, padding=6, dilation=2),
+            nn.ReLU()
+        )
+        self.decoder3 = nn.Sequential(
+            nn.ConvTranspose2d(192*2, 64*2, kernel_size=7, stride=1, padding=6, dilation=2),
+            nn.ReLU()
+        )
+        self.decoder2 = nn.Sequential(
+            nn.ConvTranspose2d(96*2, 32*2, kernel_size=7, stride=1, padding=6, dilation=2),
+            nn.ReLU()
+        )
+        self.decoder1 = nn.Sequential(
+            nn.ConvTranspose2d(32*2, 3, kernel_size=7, stride=1, padding=6, dilation=2),
+        )
+
+    def forward(self, x):
+        enc1 = self.encoder1(x)
+        enc2 = self.encoder2(enc1)
+        enc3 = self.encoder3(enc2)
+        enc4 = self.encoder4(enc3)
+
+        dec4 = self.decoder4(torch.cat((enc4, enc3), dim=1))  # Skip connection from encoder3
+        dec3 = self.decoder3(torch.cat((dec4, enc2), dim=1))  # Skip connection from encoder2
+        dec2 = self.decoder2(torch.cat((dec3, enc1), dim=1))  # Skip connection from encoder1
+        dec1 = self.decoder1(dec2)
+
+        return dec1
+
+
+class ConvAutoEncoder_simplified_90(nn.Module):
+    def __init__(self):
+        super(ConvAutoEncoder_simplified_90, self).__init__()
+
+        # Encoder
+        self.encoder1 = nn.Sequential(
+            nn.Conv2d(4, 32*2, kernel_size=7, stride=1, padding=3),
+            nn.ReLU()
+        )
+        self.encoder2 = nn.Sequential(
+            nn.Conv2d(32*2, 64*2, kernel_size=7, stride=1, padding=3),
+            nn.ReLU()
+        )
+        self.encoder3 = nn.Sequential(
+            nn.Conv2d(64*2, 128*2, kernel_size=7, stride=1, padding=3),
+            nn.ReLU()
+        )
+        self.encoder4 = nn.Sequential(
+            nn.Conv2d(128*2, 256*2, kernel_size=7, stride=1, padding=3),
+            nn.ReLU()
+        )
+
+        # Decoder
+        self.decoder4 = nn.Sequential(
+            nn.ConvTranspose2d(384*2, 128*2, kernel_size=7, stride=1, padding=3),
+            nn.ReLU()
+        )
+        self.decoder3 = nn.Sequential(
+            nn.ConvTranspose2d(192*2, 64*2, kernel_size=7, stride=1, padding=3),
+            nn.ReLU()
+        )
+        self.decoder2 = nn.Sequential(
+            nn.ConvTranspose2d(96*2, 32*2, kernel_size=7, stride=1, padding=3),
+            nn.ReLU()
+        )
+        self.decoder1 = nn.Sequential(
+            nn.ConvTranspose2d(32*2, 3, kernel_size=7, stride=1, padding=3),
+        )
+
+    def forward(self, x):
+        enc1 = self.encoder1(x)
+        enc2 = self.encoder2(enc1)
+        enc3 = self.encoder3(enc2)
+        enc4 = self.encoder4(enc3)
+
+        dec4 = self.decoder4(torch.cat((enc4, enc3), dim=1))  # Skip connection from encoder3
+        dec3 = self.decoder3(torch.cat((dec4, enc2), dim=1))  # Skip connection from encoder2
+        dec2 = self.decoder2(torch.cat((dec3, enc1), dim=1))  # Skip connection from encoder1
+        dec1 = self.decoder1(dec2)
+
+        return dec1
+
+class ConvAutoEncoder_simplified_50(nn.Module):
+    def __init__(self):
+        super(ConvAutoEncoder_simplified_50, self).__init__()
 
         # Encoder
         self.encoder1 = nn.Sequential(
@@ -63,50 +168,50 @@ class ConvAutoEncoder_simplified(nn.Module):
 
         return dec1
     
-class Convolutional_ChannelAttention_Autoencoder(nn.Module):
+class Convolutional_ChannelAttention_Autoencoder_90(nn.Module):
     def __init__(self):
-        super(Convolutional_ChannelAttention_Autoencoder, self).__init__()
+        super(Convolutional_ChannelAttention_Autoencoder_90, self).__init__()
 
         # Encoder
         self.encoder1 = nn.Sequential(
-            nn.Conv2d(4, 32, kernel_size=7, stride=1, padding=3),
+            nn.Conv2d(4, 32*2, kernel_size=7, stride=1, padding=3),
             nn.ReLU(),
-            ChannelAttention(32)
+            ChannelAttention(32*2)
         )
         self.encoder2 = nn.Sequential(
-            nn.Conv2d(32, 64, kernel_size=7, stride=1, padding=3),
+            nn.Conv2d(32*2, 64*2, kernel_size=7, stride=1, padding=3),
             nn.ReLU(),
-            ChannelAttention(64)
+            ChannelAttention(64*2)
         )
         self.encoder3 = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=7, stride=1, padding=3),
+            nn.Conv2d(64*2, 128*2, kernel_size=7, stride=1, padding=3),
             nn.ReLU(),
-            ChannelAttention(128)
+            ChannelAttention(128*2)
         )
         self.encoder4 = nn.Sequential(
-            nn.Conv2d(128, 256, kernel_size=7, stride=1, padding=3),
+            nn.Conv2d(128*2, 256*2, kernel_size=7, stride=1, padding=3),
             nn.ReLU(),
-            ChannelAttention(256)
+            ChannelAttention(256*2)
         )
 
         # Decoder
         self.decoder4 = nn.Sequential(
-            nn.ConvTranspose2d(384, 128, kernel_size=7, stride=1, padding=3),
+            nn.ConvTranspose2d(384*2, 128*2, kernel_size=7, stride=1, padding=3),
             nn.ReLU(),
-            ChannelAttention(128)
+            ChannelAttention(128*2)
         )
         self.decoder3 = nn.Sequential(
-            nn.ConvTranspose2d(192, 64, kernel_size=7, stride=1, padding=3),
+            nn.ConvTranspose2d(192*2, 64*2, kernel_size=7, stride=1, padding=3),
             nn.ReLU(),
-            ChannelAttention(64)
+            ChannelAttention(64*2)
         )
         self.decoder2 = nn.Sequential(
-            nn.ConvTranspose2d(96, 32, kernel_size=7, stride=1, padding=3),
+            nn.ConvTranspose2d(96*2, 32*2, kernel_size=7, stride=1, padding=3),
             nn.ReLU(),
-            ChannelAttention(32)
+            ChannelAttention(32*2)
         )
         self.decoder1 = nn.Sequential(
-            nn.ConvTranspose2d(32, 3, kernel_size=7, stride=1, padding=3),
+            nn.ConvTranspose2d(32*2, 3, kernel_size=7, stride=1, padding=3),
             ChannelAttention(3)
         )
 

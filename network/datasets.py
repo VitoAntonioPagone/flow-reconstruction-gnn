0b9dde6bd836149752d4f4b1fd8867a3aaaf9252
VitoPagone <cristianpagone1@gmail.com>
2023-06-07 11:48:07 +0200

    (Debug) Graph dataset class

--- network/datasets.py
+++ network/datasets.py
@@ -4,6 +4,7 @@ import torch
 import numpy as np
 import re
 
+
 class FlowDataset(Dataset):
     def __init__(self, input_dir, label_dir, add_mask=False):
         self.input_dir = input_dir
@@ -33,3 +34,36 @@ class FlowDataset(Dataset):
             input_tensor = torch.cat((input_tensor, missing_mask_tensor), dim=0)
 
         return input_tensor, label_tensor, missing_mask_tensor
+
+
+class GraphDataset(Dataset):
+    def __init__(self, root_dir, transform=None, pre_transform=None):
+        super(GraphDataset, self).__init__(root_dir, transform, pre_transform)
+
+    @property
+    def raw_file_names(self):
+        # List all files in the raw_dir
+        return os.listdir(self.raw_dir)
+
+    @property
+    def processed_file_names(self):
+        # Once processed, file names should follow this format
+        return [f"data_{i}.pt" for i in range(len(self.raw_paths))]
+    
+    def process(self):
+        # Process files one by one
+        for raw_path in self.raw_paths:
+            # Load a raw file
+            data = torch.load(raw_path)
+            
+            # Process the data into suitable format and save it
+            torch.save(data, os.path.join(self.processed_dir, f"data_{os.path.splitext(os.path.basename(raw_path))[0]}.pt"))
+
+    def len(self):
+        # The length of the dataset is simply the number of processed files
+        return len(self.processed_file_names)
+
+    def get(self, idx):
+        # Load a processed file with the given index
+        data = torch.load(os.path.join(self.processed_dir, f"data_{idx}.pt"))
+        return data

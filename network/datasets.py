1dd9fe8c58c037b46964362b123d3736b36459f3
VitoPagone <cristianpagone1@gmail.com>
2023-06-08 17:08:14 +0200

    Removed wrong graphdataset

--- network/datasets.py
+++ network/datasets.py
@@ -3,10 +3,6 @@ from torch.utils.data import Dataset
 import torch
 import numpy as np
 import re
-import torch
-from torch_geometric.data import Dataset, Data
-import os
-
 
 class FlowDataset(Dataset):
     def __init__(self, input_dir, label_dir, add_mask=False):
@@ -37,39 +33,4 @@ class FlowDataset(Dataset):
             input_tensor = torch.cat((input_tensor, missing_mask_tensor), dim=0)
 
         return input_tensor, label_tensor, missing_mask_tensor
-
-
-
-class GraphDataset(Dataset):
-    def __init__(self, input_dir, label_dir, transform=None, pre_transform=None):
-        self.input_dir = input_dir
-        self.label_dir = label_dir
-        super(GraphDataset, self).__init__(transform, pre_transform)
-
-    @property
-    def raw_file_names(self):
-        return os.listdir(self.input_dir)
-
-    @property
-    def processed_file_names(self):
-        return os.listdir(self.input_dir)
-
-    def len(self):
-        return len(self.raw_file_names)
-
-    def get(self, idx):
-        graph_file = self.raw_file_names[idx]
-        graph_path = os.path.join(self.input_dir, graph_file)
-
-        graph_data = torch.load(graph_path)
-
-        input_data = torch.load(os.path.join(self.input_dir, graph_file))
-
-        label_data = torch.load(os.path.join(self.label_dir, graph_file))
-
-        data = Data(x=input_data,
-                    edge_index=graph_data.edge_index,
-                    y=label_data)
-
-        return data
 

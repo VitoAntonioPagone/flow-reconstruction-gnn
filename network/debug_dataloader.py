f9cfb55914693a404976f2b1c063f8021ac3fee4
VitoPagone <cristianpagone1@gmail.com>
2023-06-12 14:22:36 +0200

    (DEBUG) Dataloader

--- network/debug_dataloader.py
+++ network/debug_dataloader.py
@@ -1,14 +1,18 @@
-import os
-import torch
-from torch.utils.data import Dataset
+from torch_geometric.loader import DataLoader
 from datasets import CustomDataset
-
+from utils import get_loaders_graphs 
 TRAIN_INPUT_DIR = '../dataset_graph/training/train_input_graphs'
 TRAIN_TARGET_DIR = '../dataset_graph/training/train_graphs'
 
-# Usage example:
-train_ds = CustomDataset(TRAIN_INPUT_DIR, TRAIN_TARGET_DIR)
+VALID_INPUT_DIR = '../dataset_graph/training/validation_input_graphs'  # Replace with your validation input directory
+VALID_TARGET_DIR = '../dataset_graph/training/validation_graphs'  # Replace with your validation target directory
 
-input_graph, label_graph = train_ds[0]
-print(input_graph)
-print(label_graph)
+
+# Create DataLoaders for training and validation data
+train_loader = DataLoader(TRAIN_INPUT_DIR, TRAIN_TARGET_DIR, batch_size=32)
+valid_loader = DataLoader(VALID_INPUT_DIR, VALID_TARGET_DIR, batch_size=32)
+
+# Print the type of the data loaders
+print(f'Type of train_loader: {type(train_loader)}')
+print(f'Type of valid_loader: {type(valid_loader)}')
+

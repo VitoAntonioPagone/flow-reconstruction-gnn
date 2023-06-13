8730f3311bf0233cc375032f58cc049afd1b36b3
VitoPagone <cristianpagone1@gmail.com>
2023-06-13 14:35:12 +0200

    (DEBUG) x and y need to be included as node features?

--- network/prediction_gnn.py
+++ network/prediction_gnn.py
@@ -1,10 +1,9 @@
 import torch
 from datasets import CustomDataset
-from torch_geometric.data import DataListLoader
 from models import GCN
-from collections import OrderedDict
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.interpolate import griddata
 
 # Hyperparameters
 FEAT_DIM = 4
@@ -13,11 +12,24 @@ OUTPUT_DIM = 4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Path
-TEST_INPUT_DIR = '../dataset_graph/training/test_input_graphs/graph_48_input.pt'
-TEST_TARGET_DIR = '../dataset_graph/training/test_graphs/graph_48_label.pt'
+TEST_INPUT_DIR = '../dataset_graph/training/test_input_graphs'
+TEST_TARGET_DIR = '../dataset_graph/training/test_graphs'
 CHECKPOINT_PATH = '../trained_models/GCN_epochs_10_lr_0.01_batch_32.pth.tar' 
 
+# Load dataset
 test_dataset = CustomDataset(TEST_INPUT_DIR, TEST_TARGET_DIR)
+
+# Select a single test graph
+single_graph = test_dataset[0]
+
+# Assuming that the positions are the last 2 features in the feature vector
+positions = single_graph.x[:, -2:].numpy()
+
+# Print min and max of the positions
+print(f'Min x position: {np.min(positions[:, 0])}')
+print(f'Max x position: {np.max(positions[:, 0])}')
+print(f'Min y position: {np.min(positions[:, 1])}')
+print(f'Max y position: {np.max(positions[:, 1])}')
 
 model = GCN(feat_dim=FEAT_DIM, hidden_dim=HIDDEN_DIM, output_dim=OUTPUT_DIM)
 model.to(DEVICE)
@@ -41,43 +53,47 @@ load_checkpoint(model, CHECKPOINT_PATH)
 
 model.eval()
 
-# Select one test graph
-single_graph_input = test_dataset[0]
-
-single_graph_input.x = single_graph_input.x.to(DEVICE)
-single_graph_input.edge_index = single_graph_input.edge_index.to(DEVICE)
-single_graph_input.y = single_graph_input.y.to(DEVICE)
+# Move data to the correct device
+single_graph.x = single_graph.x.to(DEVICE)
+single_graph.edge_index = single_graph.edge_index.to(DEVICE)
+single_graph.y = single_graph.y.to(DEVICE)
 
 # Perform prediction
 with torch.no_grad():
-    out = model(single_graph_input)
+    out = model(single_graph)
 
-error = (out - single_graph_input.y).abs().mean()
-print(f"Mean absolute error in predictions for test graph: {error.item()}")
+# Prepare the regular grid
+grid_x, grid_y = np.mgrid[0:1:256j, 0:1:256j]
 
 fig, axs = plt.subplots(3, 3, figsize=(18, 18))  # 3 rows for 3 channels, 3 columns for input/output/target
 
 # Assuming that the positions are the last 2 features in the feature vector
-positions = single_graph_input.x.cpu()[:, -2:].numpy()
+positions = single_graph.x.cpu()[:, -2:].numpy()
 
 for i in range(3):  # iterate over channels
-    input_values = single_graph_input.x.cpu()[:, i].numpy()
+    input_values = single_graph.x.cpu()[:, i].numpy()
     output_values = out.cpu()[:, i].numpy()
-    target_values = single_graph_input.y.cpu()[:, i].numpy()
+    target_values = single_graph.y.cpu()[:, i].numpy()
     
+    # Interpolate the values onto the regular grid
+    grid_input_values = griddata(positions, input_values, (grid_x, grid_y), method='cubic')
+    grid_output_values = griddata(positions, output_values, (grid_x, grid_y), method='cubic')
+    grid_target_values = griddata(positions, target_values, (grid_x, grid_y), method='cubic')
+
+    # Calculate and print the mean absolute error for this channel
+    error = np.mean(np.abs(output_values - target_values))
+    print(f"Mean absolute error in predictions for channel {i+1}: {error}")
+
     # Input grid
-    sc = axs[i, 0].scatter(positions[:, 0], positions[:, 1], c=input_values, cmap='jet')
-    plt.colorbar(sc, ax=axs[i, 0])
+    axs[i, 0].imshow(grid_input_values, cmap='jet')
     axs[i, 0].set_title(f'Input Channel {i+1}')
 
     # Output grid
-    sc = axs[i, 1].scatter(positions[:, 0], positions[:, 1], c=output_values, cmap='jet')
-    plt.colorbar(sc, ax=axs[i, 1])
+    axs[i, 1].imshow(grid_output_values, cmap='jet')
     axs[i, 1].set_title(f'Output Channel {i+1}')
 
     # Target grid
-    sc = axs[i, 2].scatter(positions[:, 0], positions[:, 1], c=target_values, cmap='jet')
-    plt.colorbar(sc, ax=axs[i, 2])
+    axs[i, 2].imshow(grid_target_values, cmap='jet')
     axs[i, 2].set_title(f'Target Channel {i+1}')
 
 plt.tight_layout()

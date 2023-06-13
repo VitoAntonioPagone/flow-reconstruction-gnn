1ae9a22c058e4ee2c08240802c2b79e7ae9ac4ab
VitoPagone <cristianpagone1@gmail.com>
2023-06-13 12:34:44 +0200

    (DEBUG) Adding debugging printing to see errors in the code, using only the first three features for loss calculation because the 4th one is the indicator of missing feature

--- network/train_gnn.py
+++ network/train_gnn.py
@@ -5,7 +5,6 @@ from torch.optim import Adam
 from torch_geometric.loader import DataLoader
 from datasets import CustomDataset
 from models import GCN
-import torch.nn as nn
 from collections import OrderedDict
 import os
 
@@ -17,7 +16,12 @@ BATCH_SIZE = 32
 LR = 0.01
 EPOCHS = 10
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-print(f'Device: {DEVICE}')
+LOAD_MODEL = False  # Set this to True if you want to load a pre-existing model
+MODEL_NAME = f"GCN"  
+LOAD_CHECKPOINT_FILE = f'../trained_models/{MODEL_NAME}_epochs_{EPOCHS}_lr_{LR}_batch_{BATCH_SIZE}.pth.tar'
+SAVE_CHECKPOINT_FILE = f'../trained_models/{MODEL_NAME}_epochs_{EPOCHS}_lr_{LR}_batch_{BATCH_SIZE}.pth.tar'
+
+print(f'Starting script with Device: {DEVICE}')
 
 # Paths
 TRAIN_INPUT_DIR = '../dataset_graph/training/train_input_graphs'
@@ -25,50 +29,63 @@ TRAIN_TARGET_DIR = '../dataset_graph/training/train_graphs'
 VALID_INPUT_DIR = '../dataset_graph/training/validation_input_graphs'
 VALID_TARGET_DIR = '../dataset_graph/training/validation_graphs'
 
-def save_checkpoint(state, filename="my_checkpoint.pth.tar"):
+def save_checkpoint(state, filename=SAVE_CHECKPOINT_FILE):
     print("=> Saving checkpoint")
     torch.save(state, filename)
 
-def load_checkpoint(checkpoint, model):
-    state_dict = checkpoint['state_dict']
-    new_state_dict = OrderedDict()
-    for k, v in state_dict.items():
-        name = k[7:]  # remove 'module.' from the key
-        new_state_dict[name] = v
-    model.load_state_dict(new_state_dict)
+def load_checkpoint(checkpoint, model, optimizer):
+    print("=> Loading checkpoint")
+    model.load_state_dict(checkpoint["state_dict"])
+    optimizer.load_state_dict(checkpoint["optimizer"])
 
+print('Loading train dataset...')
 train_dataset = CustomDataset(TRAIN_INPUT_DIR, TRAIN_TARGET_DIR)
-valid_dataset = CustomDataset(VALID_INPUT_DIR, VALID_TARGET_DIR)
+print('Train dataset loaded.')
 
+print('Loading validation dataset...')
+valid_dataset = CustomDataset(VALID_INPUT_DIR, VALID_TARGET_DIR)
+print('Validation dataset loaded.')
+
+print('Creating data loaders...')
 train_loader = DataLoader(train_dataset, batch_size=BATCH_SIZE, shuffle=True)
 valid_loader = DataLoader(valid_dataset, batch_size=BATCH_SIZE)
+print('Data loaders created.')
 
+print('Building model...')
 model = GCN(feat_dim=FEAT_DIM, hidden_dim=HIDDEN_DIM, output_dim=OUTPUT_DIM)
 model.to(DEVICE)
+print('Model built.')
 
 optimizer = Adam(model.parameters(), lr=LR)
 criterion = MSELoss()
 
-# Load checkpoint if exists
-if os.path.isfile("my_checkpoint.pth.tar"):
-    checkpoint = torch.load("my_checkpoint.pth.tar")
-    load_checkpoint(checkpoint, model)
+if LOAD_MODEL and os.path.isfile("my_checkpoint.pth.tar"):
+    print('Loading checkpoint...')
+    checkpoint = torch.load("my_checkpoint.pth.tar", map_location=DEVICE)
+    load_checkpoint(checkpoint, model, optimizer)
     print("Checkpoint loaded successfully.")
 
 for epoch in range(EPOCHS):
-    print(f"Starting Epoch: {epoch+1}")
+    print(f'Starting epoch {epoch + 1}...')
     model.train()
     train_loss = 0
-    for i, batch in enumerate(train_loader):
+    print('Processing training data...')
+    for batch_idx, batch in enumerate(train_loader):
         batch.x = batch.x.to(DEVICE)  
         batch.edge_index = batch.edge_index.to(DEVICE)
         batch.y = batch.y.to(DEVICE)
         optimizer.zero_grad()
         out = model(batch)
-        loss = criterion(out, batch.y)
+
+        # Calculate loss only for the first 3 features
+        loss = criterion(out[:,3], batch.y[:,3],)
+        
         train_loss += loss.item()
         loss.backward()
         optimizer.step()
+
+        # Print batch number and loss for each batch
+        print(f"  Batch {batch_idx + 1}, Training Loss: {loss.item()}")
 
     train_loss /= len(train_loader)
     print(f'Epoch: {epoch+1}, Training Loss: {train_loss}')
@@ -81,14 +98,17 @@ for epoch in range(EPOCHS):
 
     model.eval()
     valid_loss = 0
+    print('Processing validation data...')
     with torch.no_grad():
-        for i, batch in enumerate(valid_loader):
+        for batch in valid_loader:
             batch.x = batch.x.to(DEVICE)  
             batch.edge_index = batch.edge_index.to(DEVICE)
             batch.y = batch.y.to(DEVICE)
             out = model(batch)
-            loss = criterion(out, batch.y)
+            
+            # Calculate loss only for the first 3 features
+            loss = criterion(out[:,3], batch.y[:,3])
+            
             valid_loss += loss.item()
-
     valid_loss /= len(valid_loader)
     print(f'Epoch: {epoch+1}, Validation Loss: {valid_loss}')

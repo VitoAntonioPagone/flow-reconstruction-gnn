9c547a709fdebe7df2ef1b44b784920eef60c9f1
VitoPagone <cristianpagone1@gmail.com>
2023-06-17 13:48:26 +0200

    Debugging npz files

--- utils/npz_analyzer.py
+++ utils/npz_analyzer.py
@@ -13,7 +13,7 @@ def print_npz_info(npz_data):
         print(f"Data Type: {value.dtype}\n")
 
     # If the .npz file contains the expected keys, calculate the percentage of samples where all velocities are zero
-    if set(['x_velocity', 'y_velocity', 'z_velocity']).issubset(npz_data.keys()):
+    if set(['x_velocity', 'y_velocity', 'z_velocity']).issubset(npz_data.files):
         velocities = np.column_stack((npz_data['x_velocity'], npz_data['y_velocity'], npz_data['z_velocity']))
         zero_velocity_samples = np.all(velocities == 0, axis=1)
         zero_velocity_percentage = np.mean(zero_velocity_samples) * 100

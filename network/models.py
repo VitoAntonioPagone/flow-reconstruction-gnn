24c0f14006862683b2c417c4b1c833b8c493f52a
VitoPagone <cristianpagone1@gmail.com>
2023-06-29 11:41:50 +0200

    Added 99% models

--- network/models.py
+++ network/models.py
@@ -314,7 +314,7 @@ class GIN_90(torch.nn.Module):
         self.conv4 = GINConv(512, 256)
         self.conv5 = GINConv(256, 128)
         self.conv6 = GINConv(128, 64) # Updated this line
-        self.conv7 = SAGEConv(64, self.feat_dim) # New conv layer
+        self.conv7 = GINConv(64, self.feat_dim) # New conv layer
 
     def forward(self, data):
         x, edge_index = data.x, data.edge_index
@@ -583,7 +583,119 @@ class ConvNet_95_deeper(nn.Module):
 
         return dec1
 
+#### GRAPH NETWORKS ####
 
+class GAT_95(torch.nn.Module):
+    def __init__(self):
+        super(GAT_95, self).__init__()
+        self.feat_dim = 6
+        self.output_dim = 6
+        self.conv1 = GATConv(self.feat_dim, 64)
+        self.conv2 = GATConv(64, 128)
+        self.conv3 = GATConv(128, 256)
+        self.conv4 = GATConv(256, 128)
+        self.conv5 = GATConv(128, 64)
+        self.conv6 = GATConv(64, self.feat_dim)
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = self.conv1(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv2(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv3(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv4(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv5(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv6(x, edge_index)
+        return x
+
+class GraphSAGE_95(torch.nn.Module):
+    def __init__(self):
+        super(GraphSAGE_95, self).__init__()
+        self.feat_dim = 6
+        self.output_dim = 6
+        self.conv1 = SAGEConv(self.feat_dim, 128*2)
+        self.conv2 = SAGEConv(128*2, 256*2)
+        self.conv3 = SAGEConv(256*2, 512*2)
+        self.conv4 = SAGEConv(512*2, 256*2)
+        self.conv5 = SAGEConv(256*2, 128*2)
+        self.conv6 = SAGEConv(128*2, 64*2)
+        self.conv7 = SAGEConv(64*2, 32*2) # New conv layer
+        self.conv8 = SAGEConv(32*2, self.feat_dim) # New conv layer
+
+    def forward(self, data):
+        x, edge_index = data.x, data.edge_index
+        x = self.conv1(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv2(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv3(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv4(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv5(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv6(x, edge_index)
+        x = torch.relu(x)
+        x = self.conv7(x, edge_index) 
+        x = torch.relu(x)
+        x = self.conv8(x, edge_index) # New conv layer
+        return x
+#### 99% ####
+class ConvNet_99(nn.Module):
+    def __init__(self):
+        super(ConvNet_99, self).__init__()
+
+        # Encoder
+        self.encoder1 = nn.Sequential(
+            nn.Conv2d(4, 32*5, kernel_size=7, stride=1, padding=3),
+            nn.ReLU()
+        )
+        self.encoder2 = nn.Sequential(
+            nn.Conv2d(32*5, 64*5, kernel_size=7, stride=1, padding=3),
+            nn.ReLU()
+        )
+        self.encoder3 = nn.Sequential(
+            nn.Conv2d(64*5, 128*5, kernel_size=7, stride=1, padding=3),
+            nn.ReLU()
+        )
+        self.encoder4 = nn.Sequential(
+            nn.Conv2d(128*5, 256*5, kernel_size=7, stride=1, padding=3),
+            nn.ReLU()
+        )
+
+        # Decoder
+        self.decoder4 = nn.Sequential(
+            nn.ConvTranspose2d(384*5, 128*5, kernel_size=7, stride=1, padding=3),
+            nn.ReLU()
+        )
+        self.decoder3 = nn.Sequential(
+            nn.ConvTranspose2d(192*5, 64*5, kernel_size=7, stride=1, padding=3),
+            nn.ReLU()
+        )
+        self.decoder2 = nn.Sequential(
+            nn.ConvTranspose2d(96*5, 32*5, kernel_size=7, stride=1, padding=3),
+            nn.ReLU()
+        )
+        self.decoder1 = nn.Sequential(
+            nn.ConvTranspose2d(32*5, 3, kernel_size=7, stride=1, padding=3),
+        )
+
+    def forward(self, x):
+        enc1 = self.encoder1(x)
+        enc2 = self.encoder2(enc1)
+        enc3 = self.encoder3(enc2)
+        enc4 = self.encoder4(enc3)
+
+        dec4 = self.decoder4(torch.cat((enc4, enc3), dim=1))  # Skip connection from encoder3
+        dec3 = self.decoder3(torch.cat((dec4, enc2), dim=1))  # Skip connection from encoder2
+        dec2 = self.decoder2(torch.cat((dec3, enc1), dim=1))  # Skip connection from encoder1
+        dec1 = self.decoder1(dec2)
+
+        return dec1
 '''
 class DoubleConv(nn.Module):
     def __init__(self, in_channels, out_channels):

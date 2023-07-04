31d62cddf2ad5c1ad5f1f942ec41c6fdb31dda7c
VitoPagone <cristianpagone1@gmail.com>
2023-07-04 14:25:10 +0200

    Optuna

--- network/optuna_train_standard_conv.py
+++ network/optuna_train_standard_conv.py
@@ -24,27 +24,17 @@ from utils import (
 
 # Hyper-parameters
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-BATCH_SIZE = 64
-NUM_WORKERS = 6
-PIN_MEMORY = True
-LEARNING_RATE = 0.0001
-SHUFFLE = True
-NUM_EPOCHS = 200
-ALPHA = 0.1
-BETA = 0.1
 LOAD_MODEL = False  
 PERCENTAGE_OF_MISSING_POINTS = 90
-MODEL_NAME = f"ConvNet_Attention_{PERCENTAGE_OF_MISSING_POINTS}"  
-LOAD_CHECKPOINT_FILE = f'../trained_models/{MODEL_NAME}_epochs_{NUM_EPOCHS}__alpha_{ALPHA}_beta_{BETA}_lr_{LEARNING_RATE}_batch_{BATCH_SIZE}.pth.tar'
-SAVE_CHECKPOINT_FILE = f'../trained_models/{MODEL_NAME}_epochs_{NUM_EPOCHS}__alpha_{ALPHA}_beta_{BETA}_lr_{LEARNING_RATE}_batch_{BATCH_SIZE}.pth.tar'
-
+MODEL_NAME = f"Optuna_ConvNet_{PERCENTAGE_OF_MISSING_POINTS}"
+LOAD_CHECKPOINT_FILE = f'../trained_models/{MODEL_NAME}.pth.tar'
+SAVE_CHECKPOINT_FILE = f'../trained_models/{MODEL_NAME}.pth.tar'
 TRAIN_INPUTS_DIR = f'../dataset/train_data_{PERCENTAGE_OF_MISSING_POINTS}/train_inputs_{PERCENTAGE_OF_MISSING_POINTS}'
 TRAIN_LABELS_DIR = f'../dataset/train_data_{PERCENTAGE_OF_MISSING_POINTS}/train_labels_{PERCENTAGE_OF_MISSING_POINTS}'
 VAL_INPUTS_DIR   = f'../dataset/train_data_{PERCENTAGE_OF_MISSING_POINTS}/val_inputs_{PERCENTAGE_OF_MISSING_POINTS}'
 VAL_LABELS_DIR   = f'../dataset/train_data_{PERCENTAGE_OF_MISSING_POINTS}/val_labels_{PERCENTAGE_OF_MISSING_POINTS}'
 
-
-def train_fn(loader, model, optimizer, loss_fn, ns_loss, tv_loss, alpha, beta, scaler):
+def train_fn(trial, loader, model, optimizer, loss_fn, ns_loss, tv_loss, alpha, beta, scaler):
     loop = tqdm(loader, leave=True)
 
     total_loss = 0
@@ -55,24 +45,21 @@ def train_fn(loader, model, optimizer, loss_fn, ns_loss, tv_loss, alpha, beta, s
         labels = labels.to(device=DEVICE)
         mask = mask.to(device=DEVICE)
 
-        # forward
         with torch.cuda.amp.autocast():
             inputs_with_mask = torch.cat((inputs, mask), dim=1)
             outputs = model(inputs_with_mask)
             masked_loss = loss_fn(outputs, labels, mask)
             ns_loss_value = ns_loss(outputs)
-            tv_loss_value = tv_loss(outputs) # calculate TV loss
-            loss = masked_loss + alpha * ns_loss_value + beta * tv_loss_value  # added TV loss to total loss
+            tv_loss_value = tv_loss(outputs)
+            loss = masked_loss + alpha * ns_loss_value + beta * tv_loss_value
 
-        # backward
         optimizer.zero_grad()
         scaler.scale(loss).backward()
         scaler.step(optimizer)
         scaler.update()
 
-        # update tqdm loop
         loop.set_description(f"Iter [{batch_idx}/{len(loader)}]")
-        loop.set_postfix(loss=loss.item(), NSLoss=ns_loss_value.item(), MaskedL2Loss=masked_loss.item(), TVLoss=tv_loss_value.item())  # added TV loss to logging
+        loop.set_postfix(loss=loss.item(), NSLoss=ns_loss_value.item(), MaskedL2Loss=masked_loss.item(), TVLoss=tv_loss_value.item())
         
         total_loss += loss.item()
         total_batches += 1
@@ -80,67 +67,16 @@ def train_fn(loader, model, optimizer, loss_fn, ns_loss, tv_loss, alpha, beta, s
     avg_loss = total_loss / total_batches
     return avg_loss
 
-def train_unet_conv_autoencoder():
-
-    print(f"Selected device: {DEVICE}")
-
-    model = ConvNet_90().to(DEVICE)
-
-
-    # Check for multiple GPUs and wrap model
-    if torch.cuda.device_count() > 1:
-        print(f"Using {torch.cuda.device_count()} GPUs for training")
-        model = nn.DataParallel(model)
-    #print_autoencoder_dashboard(model)
-    initialize_weights(model)
-    optimizer = Adam(model.parameters(), lr=LEARNING_RATE)
-    loss_fn = MaskedMSELoss(device=DEVICE) 
-    ns_loss = NavierStokesLoss(DEVICE)
-    tv_loss = TVLoss().to(DEVICE)  # instantiate TVLoss
-
-    train_loader, val_loader = get_loaders(
-        TRAIN_INPUTS_DIR,
-        TRAIN_LABELS_DIR,
-        VAL_INPUTS_DIR,
-        VAL_LABELS_DIR,
-        BATCH_SIZE,
-        NUM_WORKERS,
-        PIN_MEMORY,
-    )
-
-    if LOAD_MODEL:
-        load_checkpoint(torch.load(LOAD_CHECKPOINT_FILE), model)
-    check_accuracy(val_loader, model, ALPHA, BETA, device=DEVICE)
-    scaler = torch.cuda.amp.GradScaler()
-
-    train_losses = []
-    val_losses = []
-
-    for epoch in range(NUM_EPOCHS):
-        print(f"Starting Epoch {epoch+1}/{NUM_EPOCHS}")
-        train_loss = train_fn(train_loader, model, optimizer, loss_fn, ns_loss, tv_loss, ALPHA, BETA, scaler) # added tv_loss
-        train_losses.append(train_loss)
-
-        # save model
-        checkpoint_state = {
-            "state_dict": model.state_dict(),
-            "optimizer": optimizer.state_dict(),
-        }
-        save_checkpoint(checkpoint_state, SAVE_CHECKPOINT_FILE)
-
-        # check accuracy
-        val_loss = check_accuracy(val_loader, model, ALPHA, BETA, device=DEVICE)
-        val_losses.append(val_loss)
-        print(f"Training Loss: {train_loss}")
-
-    plot_losses(train_losses, val_losses, ALPHA, BETA, LEARNING_RATE, BATCH_SIZE, MODEL_NAME, PERCENTAGE_OF_MISSING_POINTS, NUM_EPOCHS)
-
 def objective(trial):
     BATCH_SIZE = trial.suggest_categorical('batch_size', [32, 64, 128])
     LEARNING_RATE = trial.suggest_float('lr', 1e-5, 1e-1, log=True)
-    NUM_EPOCHS = trial.suggest_int('num_epochs', 10, 200)
-    ALPHA = trial.suggest_float('alpha', 0, 1)
-    BETA = trial.suggest_float('beta', 0, 1)
+    NUM_EPOCHS = trial.suggest_int('num_epochs', 100, 500)
+    ALPHA = trial.suggest_float('alpha', 0, 2)
+    BETA = trial.suggest_float('beta', 0, 2)
+
+    print(f"\nTrial {trial.number} parameters: \n"
+          f"Batch size: {BATCH_SIZE}, Learning rate: {LEARNING_RATE}, "
+          f"Num_epochs: {NUM_EPOCHS}, Alpha: {ALPHA}, Beta: {BETA}\n")
 
     model = ConvNet_90().to(DEVICE)
     if torch.cuda.device_count() > 1:
@@ -158,8 +94,8 @@ def objective(trial):
         VAL_INPUTS_DIR,
         VAL_LABELS_DIR,
         BATCH_SIZE,
-        NUM_WORKERS,
-        PIN_MEMORY,
+        trial.suggest_int('num_workers', 1, 8),
+        trial.suggest_categorical('pin_memory', [True, False]),
     )
 
     if LOAD_MODEL:
@@ -167,8 +103,13 @@ def objective(trial):
 
     scaler = torch.cuda.amp.GradScaler()
 
+    train_losses = []
+    val_losses = []
+
     for epoch in range(NUM_EPOCHS):
-        train_loss = train_fn(train_loader, model, optimizer, loss_fn, ns_loss, tv_loss, ALPHA, BETA, scaler)
+        print(f"\nStarting Epoch {epoch+1}/{NUM_EPOCHS}")
+        train_loss = train_fn(trial, train_loader, model, optimizer, loss_fn, ns_loss, tv_loss, ALPHA, BETA, scaler)
+        train_losses.append(train_loss)
 
         checkpoint_state = {
             "state_dict": model.state_dict(),
@@ -176,15 +117,19 @@ def objective(trial):
         }
         save_checkpoint(checkpoint_state, SAVE_CHECKPOINT_FILE)
 
-    # Assumption: check_accuracy function returns validation loss
-    val_loss = check_accuracy(val_loader, model, ALPHA, BETA, device=DEVICE)
-    return val_loss
+        val_loss = check_accuracy(val_loader, model, ALPHA, BETA, device=DEVICE)
+        val_losses.append(val_loss)
+        print(f"\nEpoch: {epoch+1}, Training Loss: {train_loss}, Validation Loss: {val_loss}\n")
+
+    plot_losses(train_losses, val_losses, ALPHA, BETA, LEARNING_RATE, BATCH_SIZE, MODEL_NAME, PERCENTAGE_OF_MISSING_POINTS, NUM_EPOCHS)
+
+    return val_losses[-1]
 
 if __name__ == "__main__":
     study = optuna.create_study(direction='minimize')
-    study.optimize(objective, n_trials=50)
+    study.optimize(objective, n_trials=100, show_progress_bar=True)
 
-    print('Best trial:')
+    print('\nBest trial:')
     trial = study.best_trial
     print('  Value: ', trial.value)
     print('  Params: ')

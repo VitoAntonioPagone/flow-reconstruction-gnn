9cefd5308c457ac19df43543fdb216f06c2579cf
VitoPagone <cristianpagone1@gmail.com>
2023-11-17 04:03:55 +0100

    New plotting style

--- network/prediction_conv_gnn.py
+++ network/prediction_conv_gnn.py
@@ -11,6 +11,9 @@ from multiprocessing import Pool
 from pykrige.ok import OrdinaryKriging
 from scipy.ndimage import gaussian_filter
 from scipy.ndimage import uniform_filter
+import matplotlib.gridspec as gridspec
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from PIL import Image
 
 from models import (red_GAT_98_6,GAT_98_8_SkipConnections,
     GAT_98_3, GAT_98_4,GAT_98_6,
@@ -24,7 +27,7 @@ from models import (red_GAT_98_6,GAT_98_8_SkipConnections,
 
 MISSING_PERCENTAGE = 98
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-CHECKPOINT_PATH = '../trained_models_FP_full/NOISY_skip_GAT_8_98_epochs_50_lr_0.0001_batch_1.pth.tar' 
+CHECKPOINT_PATH = '../trained_models_FP_full/FLUID_skip_GAT_8_98_epochs_50_lr_0.0001_batch_1.pth.tar' 
 
 def compare_positions(graph1, graph2, description):
     positions1 = graph1.x[:, -2:].cpu().numpy()
@@ -37,12 +40,14 @@ def compare_positions(graph1, graph2, description):
         max_position_difference = np.max(position_difference)
         print(f"Maximum difference in node positions between {description}: {max_position_difference}")
 
-def rmse_per_node(pred, target):
-    """Computes root mean squared error per node"""
+def rmse_per_component(pred, target):
 
-    print(pred.shape, target.shape)
+    rmse_x = torch.sqrt(torch.mean((pred[:, 0] - target[:, 0]) ** 2))
+    rmse_y = torch.sqrt(torch.mean((pred[:, 1] - target[:, 1]) ** 2))
 
-    return torch.sqrt(torch.mean((pred - target) ** 2))
+    return rmse_x.item(), rmse_y.item()
+
+
 def calculate_mean_divergence(edge_index, node_features):
     """
     Calculate the mean divergence for the entire graph.
@@ -214,15 +219,8 @@ def run_GCN(input_file, label_file):
     print("Label Graph:")
     print_graph_info(label_graph)
 
-
     # Assuming that the positions are the last 2 features in the feature vector
     positions = single_graph.x[:, -2:].numpy()
-
-    # Print min and max of the positions
-    print(f'Min x position: {np.min(positions[:, 0])}')
-    print(f'Max x position: {np.max(positions[:, 0])}')
-    print(f'Min y position: {np.min(positions[:, 1])}')
-    print(f'Max y position: {np.max(positions[:, 1])}')
 
     model = GAT_98_8_SkipConnections()
     model.to(DEVICE)
@@ -247,6 +245,9 @@ def run_GCN(input_file, label_file):
     output_graph = single_graph.clone()
     output_graph.x = predicted_features
 
+    avg_rmse_x, avg_rmse_y = rmse_per_component(predicted_features[:, :2]*7.035423, single_graph.y[:, :2]*7.035423)
+
+    print(f"Average RMSE for x-velocity: {avg_rmse_x}, y-velocity: {avg_rmse_y}")
     # Compare positions between output and target graphs
     print("\nComparing node positions between output and target graphs:")
     compare_positions(output_graph, label_graph, "output and target graphs")
@@ -258,7 +259,7 @@ def run_GCN(input_file, label_file):
     compare_positions(single_graph, label_graph, "input and target graphs")
 
     # Define grid size
-    grid_size = 600   # Increased for a smoother plot
+    grid_size = 700   # Increased for a smoother plot
 
     # Get minimum and maximum position values
     min_x, min_y = np.min(positions[:, 0]), np.min(positions[:, 1])
@@ -267,103 +268,47 @@ def run_GCN(input_file, label_file):
     # Create the grid
     grid_x, grid_y = np.mgrid[min_x:max_x:grid_size*1j, min_y:max_y:grid_size*1j]
 
-    def rmse(pred, target):
-        """Computes root mean squared error"""
-        return torch.sqrt(torch.mean((pred - target) ** 2))
+    channels = ['x-velocity', 'y-velocity']
 
-    fig, axs = plt.subplots(4, 3, figsize=(10, 10))  # Changed the subplot configuration
-    fig.subplots_adjust(hspace=0.5, wspace=0.5) 
-    
-    def mae(pred, target):
-        """Computes mean absolute error"""
-        return torch.mean(torch.abs(pred - target))
-
-    # Variables to keep track of min and max difference across all channels
-    diff_min = np.inf
-    diff_max = -np.inf
-
-    channels = ['x-velocity', 'y-velocity', 'z-velocity']
-
-    for i in range(3):
-        scaled_output_values = output_graph.x.cpu()[:, i] * 7.035423
-        scaled_target_values = single_graph.y.cpu()[:, i] * 7.035423
-        #mean_divergence = calculate_mean_divergence(single_graph.edge_index, diffused_out)
-
-        # Print the mean divergence value
-        #print("Mean divergence of the graph:", mean_divergence)
-        node_wise_rmse = rmse_per_node(scaled_output_values, scaled_target_values)
-        
-        print(f"Node-wise RMSE for channel {i}: {node_wise_rmse}")
-
-        input_values = single_graph.x.cpu()[:, i].numpy() * 7.035423
-        output_values = output_graph.x.cpu()[:, i].numpy() * 7.035423
+    target_color_ranges = []
+    for i in range(2):
         target_values = single_graph.y.cpu()[:, i].numpy() * 7.035423
+        target_color_ranges.append((np.min(target_values), np.max(target_values)))
 
-        # Replace the current interpolation method with griddata
-        grid_input_values  = griddata(positions, input_values, (grid_x, grid_y), method='nearest')
-        grid_output_values = griddata(positions, output_values, (grid_x, grid_y), method='nearest')
-        grid_output_values = gaussian_filter(grid_output_values, sigma=0.05)
+    fig = plt.figure(figsize=(24, 10))
+    gs_main = gridspec.GridSpec(2, 8, width_ratios=[1, 0.05, 1, 0.05, 1, 0.05, 1, 0.05], wspace=0.1, hspace=0.1)
 
-        grid_target_values = griddata(positions, target_values, (grid_x, grid_y), method='nearest')
+    for i in range(2):
+        for j in range(4):
+            plot_col_index = j * 2
+            ax = fig.add_subplot(gs_main[i, plot_col_index])
 
+            if j < 2:
+                values = single_graph.x.cpu()[:, i].numpy() * 7.035423 if j == 0 else output_graph.x.cpu()[:, i].numpy() * 7.035423
+                vmin, vmax = None, None
+            elif j == 2:
+                values = single_graph.y.cpu()[:, i].numpy() * 7.035423
+                vmin, vmax = target_color_ranges[i]
+            else:
+                values = output_graph.x.cpu()[:, i].numpy()  - single_graph.y.cpu()[:, i].numpy()
+                vmin, vmax = np.min(values), np.max(values)
 
-        vmin, vmax = target_values.min(), target_values.max()
-        #pixel_wise_rmse = rmse(torch.tensor(grid_output_values), torch.tensor(grid_target_values))
-        #print(f"Pixel-wise RMSE for {channels[i]}: {pixel_wise_rmse}")
-        #pixel_wise_mae = mae(torch.tensor(grid_output_values), torch.tensor(grid_target_values))
-        #print(f"Pixel-wise MAE for {channels[i]}: {pixel_wise_mae}")
+            grid_values = griddata(positions, values, (grid_x, grid_y), method='nearest')
+            cmap = 'jet' if j < 3 else 'RdBu_r'
+            im = ax.imshow(grid_values.T[::-1], extent=(min_x, max_x, min_y, max_y), origin='lower', cmap=cmap, vmin=vmin, vmax=vmax)
+            ax.set_aspect('equal', adjustable='box')
+            ax.set_title(r'${}$ {} m/s'.format(["Input", "Output", "Target", "Difference"][j], channels[i]))
+            ax.set_xticks([])
+            ax.set_yticks([])
 
-        im = axs[0, i].imshow(grid_input_values.T[::-1], extent=(min_x, max_x, min_y, max_y), origin='lower', cmap='jet', vmin=vmin, vmax=vmax)
-        axs[0, i].set_title(f'Input {channels[i]}')
-        axs[0, i].set_xticks([])
-        axs[0, i].set_yticks([])
-        cbar1 = fig.colorbar(im, ax=axs[0, i])
-        cbar1.ax.tick_params(labelsize=8)
+            divider = make_axes_locatable(ax)
+            cax = divider.append_axes("right", size="4%", pad=0.05)
+            plt.colorbar(im, cax=cax)
 
-        im = axs[1, i].imshow(grid_output_values.T[::-1], extent=(min_x, max_x, min_y, max_y), origin='lower', cmap='jet', vmin=vmin, vmax=vmax)
-        axs[1, i].set_title(f'Output {channels[i]}')
-        axs[1, i].set_xticks([])
-        axs[1, i].set_yticks([])
-        cbar2 = fig.colorbar(im, ax=axs[1, i])
-        cbar2.ax.tick_params(labelsize=8)
-
-        im = axs[2, i].imshow(grid_target_values.T[::-1], extent=(min_x, max_x, min_y, max_y), origin='lower', cmap='jet', vmin=vmin, vmax=vmax)
-        axs[2, i].set_title(f'Target {channels[i]}')
-        axs[2, i].set_xticks([])
-        axs[2, i].set_yticks([])
-        cbar3 = fig.colorbar(im, ax=axs[2, i])
-        cbar3.ax.tick_params(labelsize=8)
-
-        diff_min = min(diff_min, np.min(grid_output_values - grid_target_values))
-        diff_max = max(diff_max, np.max(grid_output_values - grid_target_values))
-
-    for i in range(3):
-        # Use the scaled data for calculating the difference
-        scaled_output_values = output_graph.x.cpu()[:, i].numpy()
-
-        scaled_target_values = single_graph.y.cpu()[:, i].numpy() 
-
-        # Calculate the difference using scaled data
-        diff_values = scaled_output_values - scaled_target_values
-
-        # Then create the grid for this difference
-        grid_diff_values = griddata(positions, diff_values, (grid_x, grid_y), method='nearest')
-        cmap = 'seismic'  # 'coolwarm', 'bwr', 'RdBu_r', and 'seismic' are all good options for diverging data
-        im = axs[3, i].imshow(grid_diff_values.T[::-1], extent=(min_x, max_x, min_y, max_y), origin='lower', cmap=cmap, vmin=diff_min, vmax=diff_max)
-        axs[3, i].set_title(f'Difference {channels[i]}')
-        axs[3, i].set_xticks([])
-        axs[3, i].set_yticks([])
-        
-        # Add colorbar with a clear label
-        cbar = fig.colorbar(im, ax=axs[3, i], orientation='vertical')
-        cbar.set_label('Difference magnitude', size=10)
-        cbar.ax.tick_params(labelsize=8)
-
-
-    plt.tight_layout()
-    fig.savefig('../results/{}_plot.png'.format(os.path.basename(CHECKPOINT_PATH).split('.')[0]), dpi=600)
-    plt.show()
-
+    filename = '../results/{}_plot.png'.format(os.path.basename(CHECKPOINT_PATH).split('.')[0])
+    fig.savefig(filename, dpi=600, bbox_inches='tight', pad_inches=0.2)
+    img = Image.open(filename)
+    img.show()
 if __name__ == "__main__":
     #input_file = f'../dataset_graph/original_data/onehundred/test_input_graphs_50/input_interpolated_input_50.pt'
     #label_file = f'../dataset_graph/original_data/onehundred/test_label_graphs_50/label_interpolated_label_50.pt'

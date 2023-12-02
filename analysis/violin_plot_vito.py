3d9a5269deb7cf9f51eb131540d4856dc8efbe1d
VitoPagone <cristianpagone1@gmail.com>
2023-12-02 13:27:24 +0100

    Violin plot vito

---
+++ analysis/violin_plot_vito.py
@@ -0,0 +1,16 @@
+import matplotlib.pyplot as plt
+import pandas as pd
+import scienceplots
+
+plt.style.use('science')
+data = pd.read_csv('../network/velocity_errors_cnn.csv', delim_whitespace=True)
+colors = ['blue', 'green', 'orange', 'red']
+fig, ax = plt.subplots(figsize=(10, 6))
+box = ax.boxplot([data['x_rmse'], data['x_mae'], data['y_rmse'], data['y_mae']],
+                 labels=['x_rmse', 'x_mae', 'y_rmse', 'y_mae'],
+                 patch_artist=True)
+ax.set_title('Box Plots for Each Metric', fontsize=16)
+ax.set_ylabel('Prediction error [m/s]', fontsize=16)
+for patch, color in zip(box['boxes'], colors):
+    patch.set_facecolor(color)
+plt.show()
